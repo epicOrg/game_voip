d758697dda606fd5d10daab9da1bd3f7cac5a5a4
rams23 <rams23@live.it>
2015-03-25 09:28:08 +0100

    Aggiunto test stream audio con inserimento manuale dei parametri di connessione

--- app/src/main/java/com/gamevoip/epicorg/gamevoip/MainActivity.java
+++ app/src/main/java/com/gamevoip/epicorg/gamevoip/MainActivity.java
@@ -6,14 +6,114 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.net.rtp.AudioCodec;
+import android.net.rtp.AudioGroup;
+import android.net.rtp.AudioStream;
+import android.net.rtp.RtpStream;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.security.acl.Group;
+
+/**
+ *
+ * Activity di test per verificare il funzionamento di AudioStream
+ *
+ *@author Luca
+ */
+
 public class MainActivity extends ActionBarActivity {
 
+    private AudioGroup audioGroup = null;
+    private AudioStream audioStream = null;
+
+
+    /**
+     *
+     * Inizializza l'AudioGroup, mixer di più AudioStream
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        audioGroup = new AudioGroup();
+        audioGroup.setMode(AudioGroup.MODE_NORMAL);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Inizializza l'AudioStream (la porta locale viene assegnata casualmente)
+     *
+     * @param view
+     */
+    public void init(View view){
+
+        try {
+
+            EditText myiptext = (EditText) findViewById(R.id.myip);
+
+            audioStream = new AudioStream(InetAddress.getByName(myiptext.getText().toString()));
+            audioStream.setCodec(AudioCodec.AMR);
+            audioStream.setMode(RtpStream.MODE_NORMAL);
+
+
+            System.out.println(audioStream.getLocalPort());
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            System.out.println(audioStream.getLocalPort());
+            Integer port = audioStream.getLocalPort();
+            textView.setText(port.toString());
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     *
+     * Associa l'AudioStream con un altro tramite IP e porta e inizia la comunicazione
+     *
+     * @param view
+     */
+    public void asociate(View view){
+
+        try {
+
+            EditText otheriptext = (EditText) findViewById(R.id.otherip);
+            EditText otherporttext = (EditText) findViewById(R.id.otherport);
+
+            audioStream.associate(InetAddress.getByName(otheriptext.getText().toString()), Integer.parseInt(otherporttext.getText().toString()));
+            audioStream.join(audioGroup);
+            AudioManager Audio =  (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            Audio.setMode(AudioManager.MODE_IN_CALL);
+            Audio.setSpeakerphoneOn(false);
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +135,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     *
+     * Attiva e disattiva il vivavoce
+     *
+     * @param view
+     */
+    public void vivavoce(View view) {
+
+        AudioManager Audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        if (Audio.isSpeakerphoneOn())
+            Audio.setSpeakerphoneOn(false);
+        else {
+            Audio.setSpeakerphoneOn(true);
+            for (int i = 0; i < 100; i++) {
+                Audio.setStreamVolume(AudioManager.STREAM_VOICE_CALL, Audio.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL),AudioManager.FLAG_SHOW_UI);
+                Audio.setStreamVolume(AudioManager.STREAM_MUSIC, Audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC),AudioManager.FLAG_SHOW_UI);
+                Audio.setStreamVolume(AudioManager.STREAM_SYSTEM, Audio.getStreamMaxVolume(AudioManager.STREAM_SYSTEM),AudioManager.FLAG_SHOW_UI);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
